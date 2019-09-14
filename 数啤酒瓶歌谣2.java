929a7227f9d7e636c2a1afa72d3d3bf5e8491b3a
frank-180 <54887365+frank-180@users.noreply.github.com>
2019-09-14 14:58:29 +0800

    Add files via upload

---
+++ 数啤酒瓶歌谣2.java
@@ -0,0 +1,31 @@
+package ÊýÆ¡¾ÆÆ¿¸èÒ¥;
+
+public class ÊýÆ¡¾ÆÆ¿¸èÒ¥2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method s
+	       int a = 99, b = 0;
+	        while(b < a) {
+	        	if(a > 1) {
+	        		System.out.println(a + " bottles of beer on the wall." + a + " bottles of beer.");
+	                System.out.println("Tske one down.");
+	                System.out.println("Pass it around.");
+	                if(a > 2) {
+	                	System.out.println(a - 1 + " bottles of beer on the wall.");
+	                }
+	                else {
+	                	System.out.println(a - 1 + " bottle of beer on the wall.");
+	                }
+	                System.out.print("\n");
+	        	}
+	        	else {
+	        		System.out.println(a + " bottle of beer on the wall." + a + " bottle of beer.");
+	        		System.out.println("Tske one down.");
+	                System.out.println("Pass it around.");
+	                System.out.println("No more bottles on the wall.");
+	        	}
+	        	a--;
+	        }
+	}
+
+}
